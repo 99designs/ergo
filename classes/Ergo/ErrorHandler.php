dadd6dce1591e4ece4adb12716d71e7288d66216
Paul Annesley <paul@99designs.com>
2009-05-25 16:41:45 +1000

    Updated errno to error string map in Ergo_ErrorHandler.

--- classes/Ergo/ErrorHandler.php
+++ classes/Ergo/ErrorHandler.php
@@ -161,21 +161,21 @@ class Ergo_ErrorHandler
 	private function _errorNumberString($intval)
 	{
 		$errorlevels = array(
-			8191 => 'E_ALL',
-			4096 => 'E_RECOVERABLE_ERROR',
-			2048 => 'E_STRICT',
-			1024 => 'E_USER_NOTICE',
-			512 => 'E_USER_WARNING',
-			256 => 'E_USER_ERROR',
-			128 => 'E_COMPILE_WARNING',
-			64 => 'E_COMPILE_ERROR',
-			32 => 'E_CORE_WARNING',
-			16 => 'E_CORE_ERROR',
-			8 => 'E_NOTICE',
-			4 => 'E_PARSE',
-			2 => 'E_WARNING',
-			1 => 'E_ERROR'
-			);
+			E_ERROR => 'E_ERROR',
+			E_WARNING => 'E_WARNING',
+			E_PARSE => 'E_PARSE',
+			E_NOTICE => 'E_NOTICE',
+			E_CORE_ERROR => 'E_CORE_ERROR',
+			E_CORE_WARNING => 'E_CORE_WARNING',
+			E_COMPILE_ERROR => 'E_COMPILE_ERROR',
+			E_COMPILE_WARNING => 'E_COMPILE_WARNING',
+			E_USER_ERROR => 'E_USER_ERROR',
+			E_USER_WARNING => 'E_USER_WARNING',
+			E_USER_NOTICE => 'E_USER_NOTICE',
+			E_ALL => 'E_ALL',
+			E_STRICT => 'E_STRICT',
+			E_RECOVERABLE_ERROR => 'E_RECOVERABLE_ERROR',
+		);
 
 		return $errorlevels[$intval];
 	}
