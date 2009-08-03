b1e0e3b0072e8393ce5b98ce1d6577877332370c
Lachlan Donald <lachlan@ljd.cc>
2009-08-03 11:57:25 +1000

    Added support for controller subdirectories to the controller locator

--- classes/Ergo/Routing/ControllerDirectory.php
+++ classes/Ergo/Routing/ControllerDirectory.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * A controller factory that uses a directory of controller files. Files should be named without
+ * a Controller suffix. Note that underscores in controller names are translated to subdirectories
+ */
 class Ergo_Routing_ControllerDirectory implements Ergo_Routing_ControllerFactory
 {
 	function __construct($dir)
@@ -14,15 +18,23 @@ class Ergo_Routing_ControllerDirectory implements Ergo_Routing_ControllerFactory
 	public function createController($name)
 	{
 		$className = sprintf('%sController',$name);
-		$fileName = "$this->_dir/$className.php";
+		$fileName = str_replace('_','/',"$this->_dir/$className.php");
 
-		if(!is_file($fileName) && !class_exists($className))
+		if(!is_file($fileName) && !class_exists($className, false))
 		{
 			throw new Ergo_Exception("Missing controller file $fileName");
 		}
-		if(!class_exists($className))
+
+		if(!class_exists($className, false))
 		{
 			require($fileName);
+
+			/*
+			if(!class_exists($className))
+			{
+				throw new Ergo_Exception("File $fileName doesn't contain $className");
+			}
+			*/
 		}
 
 		return new $className();
