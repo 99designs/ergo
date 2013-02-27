b1a7b52eca31540a1f361ae58aedb9261722ffb7
Dennis Hotson <dennis@99designs.com>
2013-02-27 03:39:18 +0000

    Added suffix parameter to \Ergo\Routing\ControllerDirectory

--- classes/Ergo/Routing/ControllerDirectory.php
+++ classes/Ergo/Routing/ControllerDirectory.php
@@ -9,12 +9,13 @@ class ControllerDirectory implements ControllerResolver
 {
 	private $_iterator;
 	private $_callback;
+	private $_suffix;
 
 	/**
 	 * @param mixed either a directory path, or an iterator
 	 * @param callback returns an instance of a controller, given file and controller name
 	 */
-	public function __construct($directoryIterator, $callback=null)
+	public function __construct($directoryIterator, $callback=null, $suffix='')
 	{
 		if(is_string($directoryIterator))
 			$directoryIterator = new \DirectoryIterator($directoryIterator);
@@ -24,6 +25,7 @@ class ControllerDirectory implements ControllerResolver
 			require_once($file);
 			return new $className();
 		};
+		$this->_suffix = $suffix;
 	}
 
 	/* (non-phpdoc)
@@ -32,8 +34,8 @@ class ControllerDirectory implements ControllerResolver
 	public function resolve($name)
 	{
 		foreach($this->_iterator as $file)
-			if($file->getFilename() == "{$name}Controller.php")
-				return call_user_func($this->_callback, (string) $file, $name.'Controller');
+			if($file->getFilename() == "{$name}{$this->_suffix}.php")
+				return call_user_func($this->_callback, (string) $file, $name.$this->_suffix);
 
 		throw new \Ergo\Exception("Unable to find a file for controller $name");
 	}
